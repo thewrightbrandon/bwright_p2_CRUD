828eaf618f915e24fe42fb573b8056210a825329
Brandon Wright <thewrightbrandon@gmail.com>
2021-04-30 20:20:19 -0400

    test

--- controllers/admin_sessions_controller.js
+++ controllers/admin_sessions_controller.js
@@ -4,7 +4,7 @@ const sessions = express.Router()
 const Admin = require('../models/admin_users.js')
 
 
-sessions.get('/new', (req, res) => {
+sessions.get('/new_admin', (req, res) => {
   res.render('sessions/sessions_new.ejs',
     {
       currentUser: req.session.currentUser,
