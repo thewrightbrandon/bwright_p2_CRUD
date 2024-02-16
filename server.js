cf6a680a9cac7ca4a1d43c6cf31722f10fdda469
Brandon Wright <thewrightbrandon@gmail.com>
2024-02-16 01:11:10 -0500

    env

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const express = require('express')
 const methodOverride  = require('method-override')
 const mongoose = require ('mongoose')
 const session = require('express-session')
-const MONGODB_URI = require('./env.js')
 
 ///////////////////////////////////
 ////// CONFIG
@@ -39,7 +38,7 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 app.use(methodOverride('_method'))
 
 app.use(session({
-    secret: "FeedMeSeymour",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
   })
@@ -49,7 +48,10 @@ app.use(session({
 ////// DATABASE
 ///////////////////////////////////
 
+const MONGODB_URI = process.env.MONGODB_URI
+
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
 db.on('disconnected', () => console.log('mongo disconnected'))
